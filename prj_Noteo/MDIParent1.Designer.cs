9f6e9cf00eee5fbb768cc685a372c21dd66ff0d7
dimitry ballot <DimitryBallot@hotmail.fr>
2019-10-11 09:50:13 +0200

    MDI

---
+++ prj_Noteo/MDIParent1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace prj_Noteo
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.trimestresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trimestreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trimestreToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.trimestreToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.totalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supprimerNoteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exporterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.réinitialiserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aJouterNoteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 533);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip.Size = new System.Drawing.Size(843, 25);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(35, 20);
+            this.toolStripStatusLabel.Text = "État";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(169, 62);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(486, 291);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trimestresToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(843, 28);
+            this.menuStrip.TabIndex = 5;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // trimestresToolStripMenuItem
+            // 
+            this.trimestresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.trimestreToolStripMenuItem,
+            this.trimestreToolStripMenuItem1,
+            this.trimestreToolStripMenuItem2,
+            this.totalToolStripMenuItem});
+            this.trimestresToolStripMenuItem.Name = "trimestresToolStripMenuItem";
+            this.trimestresToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.trimestresToolStripMenuItem.Text = "Trimestres";
+            // 
+            // trimestreToolStripMenuItem
+            // 
+            this.trimestreToolStripMenuItem.Name = "trimestreToolStripMenuItem";
+            this.trimestreToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.trimestreToolStripMenuItem.Text = "1°Trimestre";
+            // 
+            // trimestreToolStripMenuItem1
+            // 
+            this.trimestreToolStripMenuItem1.Name = "trimestreToolStripMenuItem1";
+            this.trimestreToolStripMenuItem1.Size = new System.Drawing.Size(159, 26);
+            this.trimestreToolStripMenuItem1.Text = "2°Trimestre";
+            // 
+            // trimestreToolStripMenuItem2
+            // 
+            this.trimestreToolStripMenuItem2.Name = "trimestreToolStripMenuItem2";
+            this.trimestreToolStripMenuItem2.Size = new System.Drawing.Size(159, 26);
+            this.trimestreToolStripMenuItem2.Text = "3°Trimestre";
+            // 
+            // totalToolStripMenuItem
+            // 
+            this.totalToolStripMenuItem.Name = "totalToolStripMenuItem";
+            this.totalToolStripMenuItem.Size = new System.Drawing.Size(159, 26);
+            this.totalToolStripMenuItem.Text = "Total";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.supprimerNoteToolStripMenuItem,
+            this.exporterToolStripMenuItem,
+            this.réinitialiserToolStripMenuItem,
+            this.aJouterNoteToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // supprimerNoteToolStripMenuItem
+            // 
+            this.supprimerNoteToolStripMenuItem.Name = "supprimerNoteToolStripMenuItem";
+            this.supprimerNoteToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.supprimerNoteToolStripMenuItem.Text = "Supprimer note";
+            // 
+            // exporterToolStripMenuItem
+            // 
+            this.exporterToolStripMenuItem.Name = "exporterToolStripMenuItem";
+            this.exporterToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exporterToolStripMenuItem.Text = "Exporter";
+            // 
+            // réinitialiserToolStripMenuItem
+            // 
+            this.réinitialiserToolStripMenuItem.Name = "réinitialiserToolStripMenuItem";
+            this.réinitialiserToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.réinitialiserToolStripMenuItem.Text = "Réinitialiser";
+            // 
+            // aJouterNoteToolStripMenuItem
+            // 
+            this.aJouterNoteToolStripMenuItem.Name = "aJouterNoteToolStripMenuItem";
+            this.aJouterNoteToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.aJouterNoteToolStripMenuItem.Text = "AJouter Note";
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 558);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.statusStrip);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem trimestresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trimestreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trimestreToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem trimestreToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem totalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supprimerNoteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exporterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem réinitialiserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aJouterNoteToolStripMenuItem;
+    }
+}
+
+
+
