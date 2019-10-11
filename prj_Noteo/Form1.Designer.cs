bbf276e0a6e8a7cb1b125e67a95372cbb682ee99
dimitry ballot <DimitryBallot@hotmail.fr>
2019-10-11 09:49:11 +0200

    Interface

--- prj_Noteo/Form1.Designer.cs
+++ prj_Noteo/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.llCreer = new System.Windows.Forms.LinkLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btConnexion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // llCreer
+            // 
+            this.llCreer.AutoSize = true;
+            this.llCreer.Location = new System.Drawing.Point(679, 424);
+            this.llCreer.Name = "llCreer";
+            this.llCreer.Size = new System.Drawing.Size(106, 17);
+            this.llCreer.TabIndex = 12;
+            this.llCreer.TabStop = true;
+            this.llCreer.Text = "Créer un profile";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(288, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(225, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(225, 22);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Mot de passe :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Prénom :";
+            // 
+            // btConnexion
+            // 
+            this.btConnexion.Location = new System.Drawing.Point(266, 273);
+            this.btConnexion.Name = "btConnexion";
+            this.btConnexion.Size = new System.Drawing.Size(272, 50);
+            this.btConnexion.TabIndex = 7;
+            this.btConnexion.Text = "Connexion";
+            this.btConnexion.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.llCreer);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btConnexion);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.LinkLabel llCreer;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btConnexion;
     }
 }
 
