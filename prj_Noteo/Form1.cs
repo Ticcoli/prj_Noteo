d524bde20da42f221e7adb6ed1f12815f98b3b5f
mael quenot <mael27220@outlook.fr>
2019-10-14 11:44:07 +0200

    version 1.1.4

--- prj_Noteo/Form1.cs
+++ prj_Noteo/Form1.cs
@@ -19,33 +19,6 @@ namespace prj_Noteo
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //chargement des données de la base BD_ProSante_V1 dans les collections
-            // --> lesVisiteurs
-            Globale.lesEleves = new Dictionary<int, Eleve>();
-
-            string chaineDeconnexion = "SERVER=localhost;" + "DATABASE=bulletin;" + "UID=root;" + "PASSWORD=;";
-            MySqlConnection connection = new MySqlConnection(chaineDeconnexion);
-            MySqlCommand maCommande = connection.CreateCommand();
-            MySqlDataReader maLigne;
-            maCommande.CommandText = "INSERT INTO `bulletin`.`eleve` (`ideleve`, `nom`, `prenom`, `classe`, `password`) VALUES ('4', 'tbprenom.text', 'Grégoire', 'BTS1', MD5('')); ";
-            connection.Open();
-            maLigne = maCommande.ExecuteReader();
-            while (maLigne.Read())
-            {
-                string[] valeurColonnes = new string[5];
-                for (int i = 0; i < maLigne.FieldCount; i++) valeurColonnes[i] = maLigne.GetValue(i).ToString();
-
-                string matricule = valeurColonnes[0];
-                string nom = valeurColonnes[1];
-                string prenom = valeurColonnes[2];
-                string numTel = valeurColonnes[3];
-                string Secteur = valeurColonnes[4];
-
-                visiteur unVisiteur = new visiteur(matricule, nom, prenom, int.Parse(numTel), Secteur);
-                Globale.lesVisiteurs.Add(unVisiteur);
-            }
-            connection.Close();
-
            
         }
     }
